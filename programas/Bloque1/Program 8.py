d013b17b23280944b11bd98b21516b6082108e96
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-04 02:09:24 -0600

    Create Program 8.py

---
+++ programas/Bloque1/Program 8.py
@@ -0,0 +1,7 @@
+dias = int(input("Digite la cantidad de dias: "))
+horas = dias * 24
+min = horas * 60
+meses = dias / 30
+print("La cantidad de horas en esos dias es: "+str(horas))
+print("La cantidad de minutos en esas horas es: "+str(min))
+print("La cantidad de meses en esos dias es: "+str(meses))
