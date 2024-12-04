45ddb79ade242e6aec0a1988b5d348cf6da9e206
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-04 02:08:34 -0600

    Create Program 7.py

---
+++ programas/Bloque1/Program 7.py
@@ -0,0 +1,6 @@
+radio = float(input("Ingresa el valor del radio: "))
+area = 3.1416 * radio ** 2
+perimetro = 3.1416 * 2 * radio
+print("El area del circulo es: "+str(area))
+
+print("El perimetro del circulo es: "+str(perimetro))
