de7c4a2bcaed91760a13f8dfff95f39679b60065
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-04 02:21:14 -0600

    Create Program 5.py

---
+++ programas/bloque 2/Program 5.py
@@ -0,0 +1,17 @@
+print("COMPARACIÓN:")
+print("Perro"=="Perro") #True
+print("Perro"!="Gato") #True
+print("Aguascalientes"<"Zacatecas")#True
+print("Aire">"Agua") #True
+print("\nMEMBERSHIP:")
+print("house" in "Boathouse") #True
+print("bien" in "bienvenidos")#True
+print("Y" not in "ejes")#True
+print("je" not in "ejes")#False
+print("\nSLICING:")
+print("SIRVE PARA OBTENER UN FRAGMENTO DE UNA CADENA DADA. SI SOLO DESEAMOS OBTENER UN SOLO CARACTER, ENTONCES USAREMOS LA INDEXACIÓN")
+print("INDEXING:")
+mi_nombre = "Choto Chorchis"
+print(mi_nombre[3])
+print(mi_nombre[12])
+print(mi_nombre[2:6])
