6308cda6c7c17f2466698ba7746e5f2d1df9a1f1
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-04 02:01:42 -0600

    Update and rename prueba.py to Program 1.py

---
+++ programas/Bloque1/Program 1.py
@@ -0,0 +1,4 @@
+#Fecha: 6/10/2024
+#Realizado por: Edson Raymundo Ruvalcaba Marin.
+print("EJERCICIO 1: MOSTRAR EL NOMBRE")
+print("Nombre del alumno: Edson Raymundo Ruvalcaba Marin")
--- programas/Bloque1/prueba.py
+++
@@ -1,4 +0,0 @@
-#Fecha: 6/10/2024
-#Realizado por: Fausto Alexander Ramírez Ramírez
-print("EJERCICIO 1: MOSTRAR EL NOMBRE")
-print("Nombre del alumno: Fausto Alexander Ramirez Ramirez")
