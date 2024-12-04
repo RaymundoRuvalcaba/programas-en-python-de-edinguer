cf1a1aa2902710b40cf58ecada4f5d7dc2805651
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-04 02:12:08 -0600

    Create Program 12.py

---
+++ programas/Bloque1/Program 12.py
@@ -0,0 +1,11 @@
+print("Compuerta OR")
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print("\n compuerta AND")
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
