399abb261c84b51d9a9ced912de9c6b91b8379d3
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-04 02:00:04 -0600

    Update prueba.py

--- programas/Bloque1/prueba.py
+++ programas/Bloque1/prueba.py
@@ -1,1 +1,4 @@
-jsdnsdVNVNLADSFNMBK
+#Fecha: 6/10/2024
+#Realizado por: Fausto Alexander Ramírez Ramírez
+print("EJERCICIO 1: MOSTRAR EL NOMBRE")
+print("Nombre del alumno: Fausto Alexander Ramirez Ramirez")
