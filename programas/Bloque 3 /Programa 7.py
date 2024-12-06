9eb765301ce422d3ea43b994ed5274a42b2cf38c
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:45:49 -0600

    Create Programa 7.py

---
+++ programas/Bloque 3 /Programa 7.py
@@ -0,0 +1,17 @@
+cadenas = ["Juan","Pedro","Chuy"]
+print(cadenas)  
+print(cadenas[1]) 
+print(type(cadenas)) 
+print(type(cadenas[1])) 
+
+enteros = [5, 4,8]
+print(enteros)
+print(enteros[0])
+print(type(enteros))
+print(type(enteros[0]))
+
+booleanos = [True, False, False]
+print(booleanos)
+print(booleanos[0])
+print(type(booleanos))
+print(type(booleanos[0]))
