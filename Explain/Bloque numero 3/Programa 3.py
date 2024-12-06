39bbc4a994768a79c6a263108a53379591b12d18
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 15:14:45 -0600

    Create Programa 3.py

---
+++ Explain/Bloque numero 3/Programa 3.py
@@ -0,0 +1,23 @@
+# Progrma 3.- Identificacion de tipos de datos, str, int y float
+## Fecha: 28/10/2024
+### Realizado por: Edson Raymundo Ruvalcaba Marin.
+
+- Declaramos la "Variable"
+``` python
+Variable = input("Digite su edad: ")
+```
+- Se imprime la variable declarada
+``` python
+print(type(Variable))
+```
+- Convierto la variable a tipo entero.
+``` python
+Variable = int (Variable)
+```
+- Convierto la variable a tipo flotante.
+``` python
+Variable= float(Variable)
+```
+Imprimo denuevo la variable
+``` python
+print(type(Variable))
