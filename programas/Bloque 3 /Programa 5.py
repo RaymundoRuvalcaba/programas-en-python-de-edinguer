73daa708eb458d769a2d51aee3dd7cac0930d987
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:44:26 -0600

    Create Programa 5.py

---
+++ programas/Bloque 3 /Programa 5.py
@@ -0,0 +1,5 @@
+calificacion = int(input("Digite su calificación en escala del 1 al 100: "))
+if calificacion <= 60 and calificacion <= 69: 
+    print("Su calificación es insuficiente")
+elif calificacion == 70 and calificacion <= 79: 
+    print("Su calificacion es suficiente")
