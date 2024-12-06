a5c7b2b0452a8915552509d350af7f3c04fc60dd
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:52:27 -0600

    Update and rename Programa 1 to Programa 1.py

---
+++ programas/Bloque 4/Programa 1.py
@@ -0,0 +1,10 @@
+lista_num = [10, 30, 40, 20, 35, 80]
+print(lista_num[0])
+print(lista_num[1])
+print(lista_num[2])
+print(lista_num[3])
+print(lista_num[4])
+print(lista_num[5])
+
+for i in lista_num:
+    print(i)
