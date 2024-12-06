9139b6a2e77b2bccb80ffc7c98bfe49e811a1a57
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:56:10 -0600

    Create Programa 7.py

---
+++ programas/Bloque 4/Programa 7.py
@@ -0,0 +1,6 @@
+print("THE SUM PATTERN")
+numeros = [100,200,300,400]
+sumatoria = 0 # Inicialización
+for numero in numeros:
+    sumatoria = sumatoria + numero
+print("La sumatoria es:", sumatoria)
