711786460c90d899cbefaa7bf2f72887dfee33a7
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:55:36 -0600

    Create Programa 6.py

---
+++ programas/Bloque 4/Programa 6.py
@@ -0,0 +1,6 @@
+print("THE COUNT PATTERN")
+letras = ["a", "b", "c", "d", "e"]
+contador = 0 
+for letra in letras:
+    contador = contador +1
+    print("La lista \"letras\" tiene",contador, "letras")
