5c5f0ff2d842ea2ac0bef3cef1d52378f86b3b4b
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:54:43 -0600

    Create Programa 4.py

---
+++ programas/Bloque 4/Programa 4.py
@@ -0,0 +1,19 @@
+print("Imprime los valores del 0 al 9")
+x = range(10)
+for num in x:
+    print(num)
+
+print("Imprime los valores del 5 al 15")
+x1 = range(5,16)
+for num in x1:
+    print(num)
+
+print("Imprime los pares del 10 al 20")
+x2 = range(10,21,2)
+for num in x2:
+    print(num)
+
+print("Imprime los impares del 10 al 20")
+x3 = range(11,22,2)
+for num in x3:
+    print(num)
