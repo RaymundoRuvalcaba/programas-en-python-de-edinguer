4714d9c423c304b22eac1c8f15faee32a1f3b736
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:36:43 -0600

    Create Programa 2.py

---
+++ programas/Bloque 3 /Programa 2.py
@@ -0,0 +1,8 @@
+mascota= input("Ingresa el tipo de mascota que tienes: ")
+if "perro"in mascota:
+    print("Es un perro: ")
+elif "gato" in mascota:
+    print("Tienes un gato: ")
+else:
+    print("Tipo de mascota desconocida")
+    print("¡Gracias por usar nuestro programa!")
