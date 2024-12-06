c96e154ba1926eab55552e09c70a4b76ecd0d953
Edson Raymundo Ruvalcaba Marin <redsonn@outlook.es>
2024-12-06 14:36:04 -0600

    Update and rename j to Programa 1.py

---
+++ programas/Bloque 3 /Programa 1.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite el primer numero en numeros enteros: "))
+n2 = int(input("Digite el segundo numero en numeros enteros: "))
+
+if n1 > n2:
+    print("El numero " +str(n1) + " es mayor al " +str(n2))
+elif n1 == n2:
+    print("Son numeros iguales")
+else:
+    print("El numero " +str(n1)+ " es menor al numero"+str(n2))
+
+    print("¡Gracias por usar nuestro programa!")
